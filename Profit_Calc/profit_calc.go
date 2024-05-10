288fd0bc16d4aacd41bd12cf44a454ff1c32504c
James Howes <zhowes14@icloud.com>
2024-05-10 19:29:14 -0400

    my changes to match course

--- Profit_Calc/profit_calc.go
+++ Profit_Calc/profit_calc.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
 func main() {
@@ -10,15 +12,29 @@ func main() {
 	taxRate := 5.5
 	var expenses float64
 
-	requestInput("Revenue", &revenue)
+	err := requestInput("Revenue", &revenue)
 
-	requestInput("Expenes", &expenses)
+	if err != nil {
+		fmt.Println("there was an error in main")
+		fmt.Println(err)
+		panic("can't continue. Sorry")
+	}
 
-	requestInput("Tax Rate", &taxRate)
+	err = requestInput("Expenes", &expenses)
 
-	fmt.Println(revenue)
-	fmt.Println(expenses)
-	fmt.Println(taxRate)
+	if err != nil {
+		fmt.Println("there was an error in main")
+		fmt.Println(err)
+		panic("Can't continue. Sorry")
+	}
+
+	err = requestInput("Tax Rate", &taxRate)
+
+	if err != nil {
+		fmt.Println("there was an error in main")
+		fmt.Println(err)
+		panic("Can't continue. Sorry")
+	}
 
 	ebt, profit, ratio := calcFinanceInfo(revenue, expenses, taxRate)
 
@@ -26,11 +42,21 @@ func main() {
 	fmt.Printf("Profit: %.1f\n", profit)
 	fmt.Printf("Ratio: %.3f\n", ratio)
 
+	writeInfoToFile(ebt, profit, ratio)
+
 }
 
-func requestInput(question string, value *float64) {
+func requestInput(question string, value *float64) error {
 	fmt.Printf("%s: ", question)
 	fmt.Scan(value)
+
+	if *value == 0 {
+		return errors.New("value was 0")
+	} else if *value < 0 {
+		return errors.New("value was negitive")
+	}
+
+	return nil
 }
 
 func calcFinanceInfo(revenue float64, expenses float64, taxRate float64) (float64, float64, float64) {
@@ -38,10 +64,12 @@ func calcFinanceInfo(revenue float64, expenses float64, taxRate float64) (float6
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
 
-	fmt.Println(ebt)
-	fmt.Println(profit)
-	fmt.Println(ratio)
-
 	return ebt, profit, ratio
+
+}
+
+func writeInfoToFile(ebt float64, profit float64, ratio float64) {
+	finText := fmt.Sprintf("EBT: %.2f\nProfit: %.2f\nRatio: %.2f\n", ebt, profit, ratio)
+	os.WriteFile("finance.txt", []byte(finText), 0644)
 
 }
