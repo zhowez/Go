a86407c3ace80fd93e40de77b82226c647652259
James Howes <zhowes14@icloud.com>
2024-06-13 16:20:40 -0400

    cloture

--- functions/main.go
+++ functions/main.go
@@ -2,42 +2,39 @@ package main
 
 import "fmt"
 
-type transformFn func(int) int
 func main() {
+	numbers := []int{1, 2, 3}
+	
+	double := createTransformer(2)
+	triple := createTransformer(3)
 
-	numbers := []int{1,2,3,4}
+	transformed := transformNumbers(&numbers, func (number int) int { return number * 2 })
 
 	doubled := transformNumbers(&numbers, double)
-	trippled := transformNumbers(&numbers, tripple)
 
-	fmt.Println(numbers)
+	tripled := transformNumbers(&numbers, triple)
 
 	fmt.Println(doubled)
 
-	fmt.Println(trippled)
+	fmt.Println(tripled)
 
 
-
+	fmt.Println(transformed)
 }
 
-
-func transformNumbers (numbers *[]int, transform transformFn) []int{
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
-	for _, val:= range *numbers {
-		dNumbers = append(dNumbers,transform(val))
+
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
 	}
 
 	return dNumbers
 }
 
-func getTransformerFunction() transformFn {
-	return double
-}
 
-func double(number int) int {
-	return number * 2
-}
-
-func tripple(number int) int {
-	return number * 3
+func createTransformer(factor int)  func(int) int{ 
+	return func(number int) int {
+		return number * factor
+	}
 }
